4c920eca5c6550ea3a4318d20fe91285bb23397f
Vidit Tamrakar <vidit.tamrakar16@gmail.com>
2024-04-21 03:12:28 +0530

    up

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,9 +1,35 @@
-
+"use client"
+import Home from '@repo/ui/home'
+import { useState, useEffect } from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
 
 export default function Page(): JSX.Element {
+
+  const [loading, setloading] = useState(true)
+
+  useEffect(()=>{
+   
+    setTimeout(()=>{
+      setloading(false)
+    }, 2000)
+  })
+  
+
+  
   return (
-    <div>
-      
-    </div>
+   
+   <>
+   {loading? 
+    
+    <div className='flex items-center justify-center h-[920px] w-[100%] bg-[white] flex-col'>
+      <ClipLoader color="#575958" loading={true} />
+      <h1 className="text-[32px] font-[600] text-[#505050a8] mt-3">WalletPe</h1>
+    </div>: 
+  
+    <Home></Home>
+  
+  }
+   </> 
+
     );
 }
