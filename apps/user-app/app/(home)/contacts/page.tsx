48e7a461c2438970067d3bef7bd5a089ce96cb9c
Vidit Tamrakar <vidit.tamrakar16@gmail.com>
2024-05-07 00:31:58 +0530

    up

---
+++ apps/user-app/app/(home)/contacts/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+
+export default function Contacts () {
+    return (
+        <div className="h-[930px] w-[100%] bg-[#ebebeb]  select-none flex items-center justify-center">
+            <div className="h-[600px] w-[800px]  bg-[#f0f0f0] shadow-lg  flex items-center justify-start flex-col overflow-hidden overflow-y-scroll ">
+
+                <div className="mt-3 min-h-[60px] w-[400px] bg-[white] rounded-[18px] flex items-center justify-start mb-2">
+
+                    <div className="ml-3 h-[50px] w-[50px] rounded-[50%] bg-[purple] text-[white] flex items-center justify-center">
+                            V
+                    </div>
+
+                    <h2 className="ml-3">Vidit Tamrakar</h2>
+                </div>
+
+                
+                
+
+
+            </div>
+
+            <div className= "relative bottom-[160px] left-[30px] h-[270px] w-[500px] bg-[white] rounded-[18px] flex justify-center items-center flex-col shadow-lg">
+                        <h1 className="text-[19px]">Add Contact</h1>
+                        <input  type="number" className="mt-3 h-[35px] w-[360px] border border-[gray]" placeholder="&nbsp;Enter Phone Number" />
+                        <h3 className="mt-2">OR</h3>
+                        <input  type="text" className="mt-3 h-[35px] w-[360px] border border-[gray]" placeholder="&nbsp;Enter WalletPe Id" />
+                        
+                        <button className="mt-5 h-[40px] w-[360px] bg-[#11E980] text-[white] cursor-pointer">Add</button>
+                      
+                    </div>
+            
+        </div>
+    )
+}
--- packages/ui/src/sidebar.tsx
+++ packages/ui/src/sidebar.tsx
@@ -24,12 +24,13 @@ export default function Sidebar(props: propstype) {
              </div>
 
 
-             <div onClick={()=>{handlemove('/rewards')}} className="h-[45px] w-[185px] flex justify-start items-center mt-[20px] cursor-pointer">
+             <div onClick={()=>{handlemove('/contacts')}} className="h-[45px] w-[185px] flex justify-start items-center mt-[20px] cursor-pointer">
              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="gray" viewBox="0 0 16 16">
-             <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v2h6a.5.5 0 0 1 .5.5c0 .253.08.644.306.958.207.288.557.542 1.194.542s.987-.254 1.194-.542C9.42 6.644 9.5 6.253 9.5 6a.5.5 0 0 1 .5-.5h6v-2A1.5 1.5 0 0 0 14.5 2z"/>
-             <path d="M16 6.5h-5.551a2.7 2.7 0 0 1-.443 1.042C9.613 8.088 8.963 8.5 8 8.5s-1.613-.412-2.006-.958A2.7 2.7 0 0 1 5.551 6.5H0v6A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5z"/>
+             <path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5"/>
+             <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2"/>
+             <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1z"/>
              </svg>
-             <h2 className="text-[19px] ml-5">Rewards</h2>
+             <h2 className="text-[19px] ml-5">Contacts</h2>
              </div>
 
 
