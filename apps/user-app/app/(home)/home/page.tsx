b93dcd28332bbcae3af543b654d7b779494d5736
Vidit Tamrakar <vidit.tamrakar16@gmail.com>
2024-05-09 01:16:04 +0530

    up

--- apps/user-app/app/(home)/home/page.tsx
+++ apps/user-app/app/(home)/home/page.tsx
@@ -121,9 +121,11 @@ export default function Home () {
             <div onClick={()=>{handlepay(false,true)}} className=" h-[70px] w-[600px] mt-[30px] bg-[white] rounded-[18px] absolute top-[350px] left-[1030px] flex items-center justify-start select-none cursor-pointer  shadow-lg">
                 <h2 className="ml-4 text-[purple]" >Pay Contacts</h2>
 
-                <div className="ml-5 text-[19px] text-[purple]">
-                    @
-                </div>
+                
+                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="purple" className="ml-5" viewBox="0 0 16 16">
+                 <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z"/>
+                </svg>
+               
             </div>
 
 
